17bb6a0c22525ffd17d6ac05c05638072a6f876b
LuoTat <LuoTat.s@gmail.com>
2024-09-08 10:11:18 +0800

    重构了in_strings_EventCommand方法，修改了一些局部变量名称，增加了Actors，Armors，Classes，Enemies，Items，Skills，States，Tilesets，Weapons的note属性的注入

--- inject_strings.rb
+++ inject_strings.rb
@@ -2,60 +2,62 @@ require 'oj'
 require 'fileutils'
 require_relative 'RGSS3'
 
-def in_strings_EventCommand(eventcommand)
-    case eventcommand.code
-    when 102 # 显示选项
-        eventcommand.parameters[0].map! do |string|
-            next string if string.empty?
-            new_string = $all_ex_strings[string]
-            string = (new_string.nil? || new_string.empty?) ? string : new_string
+def in_strings_EventCommandList(eventCommandList)
+    eventCommandList.each do |eventCommand|
+        case eventCommand.code
+        when 102 # 显示选项
+            eventCommand.parameters[0].map! do |string|
+                next string if string.empty?
+                new_string = $all_ex_strings[string]
+                string = (new_string.nil? || new_string.empty?) ? string : new_string
+            end
+        when 108 # 注释
+            new_string = $all_ex_strings[eventCommand.parameters[0]]
+            eventCommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
+        when 111 # 如果_分支
+            if eventCommand.parameters[0] == 4 && eventCommand.parameters[2] == 1
+                new_string = $all_ex_strings[eventCommand.parameters[3]]
+                eventCommand.parameters[3] = new_string unless new_string.nil? || new_string.empty?
+            elsif eventCommand.parameters[0] == 12
+                new_string = $all_ex_strings[eventCommand.parameters[1]]
+                eventCommand.parameters[1] = new_string unless new_string.nil? || new_string.empty?
+            end
+        when 118 # 标签
+            new_string = $all_ex_strings[eventCommand.parameters[0]]
+            eventCommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
+        when 119 # 转至标签
+            new_string = $all_ex_strings[eventCommand.parameters[0]]
+            eventCommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
+        when 122 # 变量操作
+            if eventCommand.parameters[3] == 4
+                new_string = $all_ex_strings[eventCommand.parameters[4]]
+                eventCommand.parameters[4] = new_string unless new_string.nil? || new_string.empty?
+            end
+        when 320 # 更改名字
+            new_string = $all_ex_strings[eventCommand.parameters[1]]
+            eventCommand.parameters[1] = new_string unless new_string.nil? || new_string.empty?
+        when 324 # 更改称号
+            new_string = $all_ex_strings[eventCommand.parameters[1]]
+            eventCommand.parameters[1] = new_string unless new_string.nil? || new_string.empty?
+        when 355 # 脚本
+            new_string = $all_ex_strings[eventCommand.parameters[0]]
+            eventCommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
+        when 401 # 显示文章的字符串
+            new_string = $all_ex_strings[eventCommand.parameters[0]]
+            eventCommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
+        when 402 # 如果选择
+            new_string = $all_ex_strings[eventCommand.parameters[1]]
+            eventCommand.parameters[1] = new_string unless new_string.nil? || new_string.empty?
+        when 405 # 显示滚动文字的字符串
+            new_string = $all_ex_strings[eventCommand.parameters[0]]
+            eventCommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
+        when 408 # 注释的字符串
+            new_string = $all_ex_strings[eventCommand.parameters[0]]
+            eventCommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
+        when 655 # 脚本的字符串
+            new_string = $all_ex_strings[eventCommand.parameters[0]]
+            eventCommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
         end
-    when 108 # 注释
-        new_string = $all_ex_strings[eventcommand.parameters[0]]
-        eventcommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
-    when 111 # 如果_分支
-        if eventcommand.parameters[0] == 4 && eventcommand.parameters[2] == 1
-            new_string = $all_ex_strings[eventcommand.parameters[3]]
-            eventcommand.parameters[3] = new_string unless new_string.nil? || new_string.empty?
-        elsif eventcommand.parameters[0] == 12
-            new_string = $all_ex_strings[eventcommand.parameters[1]]
-            eventcommand.parameters[1] = new_string unless new_string.nil? || new_string.empty?
-        end
-    when 118 # 标签
-        new_string = $all_ex_strings[eventcommand.parameters[0]]
-        eventcommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
-    when 119 # 转至标签
-        new_string = $all_ex_strings[eventcommand.parameters[0]]
-        eventcommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
-    when 122 # 变量操作
-        if eventcommand.parameters[3] == 4
-            new_string = $all_ex_strings[eventcommand.parameters[4]]
-            eventcommand.parameters[4] = new_string unless new_string.nil? || new_string.empty?
-        end
-    when 320 # 更改名字
-        new_string = $all_ex_strings[eventcommand.parameters[1]]
-        eventcommand.parameters[1] = new_string unless new_string.nil? || new_string.empty?
-    when 324 # 更改称号
-        new_string = $all_ex_strings[eventcommand.parameters[1]]
-        eventcommand.parameters[1] = new_string unless new_string.nil? || new_string.empty?
-    when 355 # 脚本
-        new_string = $all_ex_strings[eventcommand.parameters[0]]
-        eventcommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
-    when 401 # 显示文章的字符串
-        new_string = $all_ex_strings[eventcommand.parameters[0]]
-        eventcommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
-    when 402 # 如果选择
-        new_string = $all_ex_strings[eventcommand.parameters[1]]
-        eventcommand.parameters[1] = new_string unless new_string.nil? || new_string.empty?
-    when 405 # 显示滚动文字的字符串
-        new_string = $all_ex_strings[eventcommand.parameters[0]]
-        eventcommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
-    when 408 # 注释的字符串
-        new_string = $all_ex_strings[eventcommand.parameters[0]]
-        eventcommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
-    when 655 # 脚本的字符串
-        new_string = $all_ex_strings[eventcommand.parameters[0]]
-        eventcommand.parameters[0] = new_string unless new_string.nil? || new_string.empty?
     end
 end
 
@@ -63,8 +65,10 @@ def in_strings_Actors(actors)
     actors[1..-1].each do |actor|
         new_name = $all_ex_strings[actor.name]
         new_nickname = $all_ex_strings[actor.nickname]
+        new_note = $all_ex_strings[actor.note]
         actor.name = new_name unless new_name.nil? || new_name.empty?
         actor.nickname = new_nickname unless new_nickname.nil? || new_nickname.empty?
+        actor.note = new_note unless new_note.nil? || new_note.empty?
     end
 end
 
@@ -79,43 +83,47 @@ def in_strings_Armors(armors)
     armors[1..-1].each do |armor|
         new_name = $all_ex_strings[armor.name]
         new_description = $all_ex_strings[armor.description]
+        new_note = $all_ex_strings[armor.note]
         armor.name = new_name unless new_name.nil? || new_name.empty?
         armor.description = new_description unless new_description.nil? || new_description.empty?
+        armor.note = new_note unless new_note.nil? || new_note.empty?
     end
 end
 
 def in_strings_Classes(klasses)
     klasses[1..-1].each do |klass|
         new_name = $all_ex_strings[klass.name]
+        new_note = $all_ex_strings[klass.note]
         klass.name = new_name unless new_name.nil? || new_name.empty?
+        klass.note = new_note unless new_note.nil? || new_note.empty?
     end
 end
 
 def in_strings_CommonEvents(commonEvents)
-    commonEvents[1..-1].each do |commonevent|
-        new_name = $all_ex_strings[commonevent.name]
-        commonevent.name = new_name unless new_name.nil? || new_name.empty?
-        commonevent.list.each do |eventcommand|
-            in_strings_EventCommand(eventcommand)
-        end
+    commonEvents[1..-1].each do |commonEvent|
+        new_name = $all_ex_strings[commonEvent.name]
+        commonEvent.name = new_name unless new_name.nil? || new_name.empty?
+        in_strings_EventCommandList(commonEvent.list)
     end
 end
 
 def in_strings_Enemies(enemies)
     enemies[1..-1].each do |enemy|
         new_name = $all_ex_strings[enemy.name]
+        new_note = $all_ex_strings[enemy.note]
         enemy.name = new_name unless new_name.nil? || new_name.empty?
-        if enemy.instance_variable_defined?(:@data_ex) # 判断是否存在@data_ex实例变量
-            ex_data = enemy.instance_variable_get(:@data_ex)
-            if ex_data.key?(:lib_name) # 判断是否存在lib_name键
-                new_lib_name = $all_ex_strings[ex_data[:lib_name]]
-                ex_data[:lib_name] = new_lib_name unless new_lib_name.nil? || new_lib_name.empty?
-            end
-            if ex_data.key?(:lib_category) # 判断是否存在lib_category键
-                new_lib_category = $all_ex_strings[ex_data[:lib_category]]
-                ex_data[:lib_category] = new_lib_category unless new_lib_category.nil? || new_lib_category.empty?
-            end
-        end
+        enemy.note = new_note unless new_note.nil? || new_note.empty?
+        # if enemy.instance_variable_defined?(:@data_ex) # 判断是否存在@data_ex实例变量
+        #     ex_data = enemy.instance_variable_get(:@data_ex)
+        #     if ex_data.key?(:lib_name) # 判断是否存在lib_name键
+        #         new_lib_name = $all_ex_strings[ex_data[:lib_name]]
+        #         ex_data[:lib_name] = new_lib_name unless new_lib_name.nil? || new_lib_name.empty?
+        #     end
+        #     if ex_data.key?(:lib_category) # 判断是否存在lib_category键
+        #         new_lib_category = $all_ex_strings[ex_data[:lib_category]]
+        #         ex_data[:lib_category] = new_lib_category unless new_lib_category.nil? || new_lib_category.empty?
+        #     end
+        # end
     end
 end
 
@@ -123,8 +131,10 @@ def in_strings_Items(items)
     items[1..-1].each do |items|
         new_name = $all_ex_strings[items.name]
         new_description = $all_ex_strings[items.description]
+        new_note = $all_ex_strings[items.note]
         items.name = new_name unless new_name.nil? || new_name.empty?
         items.description = new_description unless new_description.nil? || new_description.empty?
+        items.note = new_note unless new_note.nil? || new_note.empty?
     end
 end
 
@@ -135,9 +145,7 @@ def in_strings_Map(map)
         new_name = $all_ex_strings[event.name]
         event.name = new_name unless new_name.nil? || new_name.empty?
         event.pages.each do |page|
-            page.list.each do |eventcommand|
-                in_strings_EventCommand(eventcommand)
-            end
+            in_strings_EventCommandList(page.list)
         end
     end
 end
@@ -155,10 +163,12 @@ def in_strings_Skills(skills)
         new_description = $all_ex_strings[skill.description]
         new_message1 = $all_ex_strings[skill.message1]
         new_message2 = $all_ex_strings[skill.message2]
+        new_note = $all_ex_strings[skill.note]
         skill.name = new_name unless new_name.nil? || new_name.empty?
         skill.description = new_description unless new_description.nil? || new_description.empty?
         skill.message1 = new_message1 unless new_message1.nil? || new_message1.empty?
         skill.message2 = new_message2 unless new_message2.nil? || new_message2.empty?
+        skill.note = new_note unless new_note.nil? || new_note.empty?
     end
 end
 
@@ -169,11 +179,13 @@ def in_strings_States(states)
         new_message2 = $all_ex_strings[state.message2]
         new_message3 = $all_ex_strings[state.message3]
         new_message4 = $all_ex_strings[state.message4]
+        new_note = $all_ex_strings[state.note]
         state.name = new_name unless new_name.nil? || new_name.empty?
         state.message1 = new_message1 unless new_message1.nil? || new_message1.empty?
         state.message2 = new_message2 unless new_message2.nil? || new_message2.empty?
         state.message3 = new_message3 unless new_message3.nil? || new_message3.empty?
         state.message4 = new_message4 unless new_message4.nil? || new_message4.empty?
+        state.note = new_note unless new_note.nil? || new_note.empty?
     end
 end
 
@@ -227,7 +239,9 @@ end
 def in_strings_Tilesets(tilesets)
     tilesets[1..-1].each do |tileset|
         new_name = $all_ex_strings[tileset.name]
+        new_note = $all_ex_strings[tileset.note]
         tileset.name = new_name unless new_name.nil? || new_name.empty?
+        tileset.note = new_note unless new_note.nil? || new_note.empty?
     end
 end
 
@@ -242,8 +256,10 @@ def in_strings_Weapons(weapons)
     weapons[1..-1].each do |weapon|
         new_name = $all_ex_strings[weapon.name]
         new_description = $all_ex_strings[weapon.description]
+        new_note = $all_ex_strings[weapon.note]
         weapon.name = new_name unless new_name.nil? || new_name.empty?
         weapon.description = new_description unless new_description.nil? || new_description.empty?
+        weapon.note = new_note unless new_note.nil? || new_note.empty?
     end
 end
 
@@ -269,11 +285,11 @@ $all_ex_strings = Oj.load(File.read('ManualTransFile.json'))
     'Data/Troops.rvdata2',
     'Data/Weapons.rvdata2'
 ].each do |rvdata2path|
-    puts rvdata2path
-    rvdatafilebasename = File.basename(rvdata2path, '.*')
+    print "Injecting strings to \e[32m#{rvdata2path}\e[0m...\n"
+    rvdata2basename = File.basename(rvdata2path, '.*')
     File.open(rvdata2path, 'rb') do |rvdata2file|
         object = Marshal.load(rvdata2file.read)
-        case rvdatafilebasename
+        case rvdata2basename
         when 'Actors'
             in_strings_Actors(object)
         when 'Animations'
@@ -307,7 +323,7 @@ $all_ex_strings = Oj.load(File.read('ManualTransFile.json'))
         end
 
         # 将替换后的object序列化到新的rvdata2文件中
-        File.open('Data_New/' + rvdatafilebasename + '.rvdata2', 'wb') do |rvdata2file|
+        File.open('Data_New/' + rvdata2basename + '.rvdata2', 'wb') do |rvdata2file|
             rvdata2file.write(Marshal.dump(object))
         end
     end
