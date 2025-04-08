88221b890ee6968a94a69710da77eb26366c9d5a
LuoTat <LuoTat.s@gmail.com>
2025-04-09 00:11:50 +0800

    为了支持递归遍历所有的*.json文件，修改了函数ex_strings的逻辑

--- lib/R3EXS/extract_strings.rb
+++ lib/R3EXS/extract_strings.rb
@@ -7,34 +7,10 @@ require_relative 'RGSS3_R3EXS'
 
 module R3EXS
 
-    # 将 Ruby 源码中的字符串和符号提取出来
-    #
-    # @param target_dir [String] 目标目录
-    # @param with_symbol [Boolean] 是否包含脚本中的符号
-    #
-    # @raise [ScriptsDirError] Scripts 目录不存在
-    #
-    # @return [Array<String>]
-    def R3EXS.rb_ex_strings(target_dir, with_symbol)
-        full_dir = File.join(target_dir, 'Scripts')
-        Dir.exist?(full_dir) or raise ScriptsDirError.new(full_dir), "Scripts directory not found: #{full_dir}"
-
-        strings           = []
-        strings_extractor = StringsExtractor.new(strings, with_symbol)
-        Dir.glob(File.join(full_dir, '*.rb')).each do |script_file_path|
-            print "#{Utils::ESCAPE}#{Utils::MAGENTA_COLOR}Extracting from #{Utils::RESET_COLOR}#{script_file_path}...\r" if $global_options[:verbose]
-
-            strings_extractor.visit(Prism.parse_file(script_file_path).value)
-
-            print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Extracted #{Utils::RESET_COLOR}#{script_file_path}\n" if $global_options[:verbose]
-        end
-        strings
-    end
-
     # 将指定目录下的所有已经序列化为 R3EXS 后的 JOSN 文件中的字符串提取出来
     #
-    # @param target_dir [String] 目标目录
-    # @param output_dir [String] 输出目录
+    # @param target_dir [Pathname] 目标目录
+    # @param output_dir [Pathname] 输出目录
     # @param with_scripts [Boolean] 是否包含脚本
     # @param with_symbol [Boolean] 是否包含脚本中的符号
     # @param with_scripts_separate [Boolean] 是否将脚本提取的字符串单独存放
@@ -45,11 +21,11 @@ module R3EXS
     #
     # @return [void]
     def R3EXS.ex_strings(target_dir, output_dir, with_scripts, with_symbol, with_scripts_separate)
-        FileUtils.mkdir(output_dir) unless Dir.exist?(output_dir)
         all_ex_strings = []
 
-        Utils.all_json_files(target_dir, :R3EXS) do |object, file_basename|
-            file_path = File.join(target_dir, "#{file_basename}.json")
+        # 处理常规的 JSON 文件
+        Utils.all_json_files(target_dir, :R3EXS) do |object, file_basename, parent_relative_dir|
+            file_path = target_dir.join(parent_relative_dir, "#{file_basename}.json")
             print "#{Utils::ESCAPE}#{Utils::MAGENTA_COLOR}Extracting from #{Utils::RESET_COLOR}#{file_path}...\r" if $global_options[:verbose]
             if object.is_a?(Array)
                 object.each do |obj|
@@ -58,23 +34,47 @@ module R3EXS
             else
                 all_ex_strings.concat(object.ex_strings)
             end
-            print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Extracted #{Utils::RESET_COLOR}#{file_basename}\n" if $global_options[:verbose]
+            print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Extracted #{Utils::RESET_COLOR}#{file_path}\n" if $global_options[:verbose]
         end
 
+        # 处理 CommonEvent_\d{5}.json 文件
+        Utils.all_commonevent_json_files(target_dir, :R3EXS) do |commonevents, parent_relative_dir|
+            file_path = target_dir.join(parent_relative_dir, "CommonEvent_*.json")
+            print "#{Utils::ESCAPE}#{Utils::MAGENTA_COLOR}Extracting from #{Utils::RESET_COLOR}#{file_path}...\r" if $global_options[:verbose]
+            commonevents.each do |commonevent|
+                all_ex_strings.concat(commonevent.ex_strings)
+            end
+            print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Extracted #{Utils::RESET_COLOR}#{file_path}\n" if $global_options[:verbose]
+        end
+
+        # 处理 *.rb 文件
         if with_scripts
-            scripts_strings = rb_ex_strings(target_dir, with_symbol)
+            all_rb_strings    = []
+            strings_extractor = StringsExtractor.new(all_rb_strings, with_symbol)
+
+            Utils.all_rb_files(target_dir) do |scripts, parent_relative_dir|
+                file_path = target_dir.join(parent_relative_dir, "*.rb")
+                print "#{Utils::ESCAPE}#{Utils::MAGENTA_COLOR}Extracting from #{Utils::RESET_COLOR}#{file_path}...\r" if $global_options[:verbose]
+
+                scripts.each do |script|
+                    strings_extractor.visit(Prism.parse(script).value)
+                end
+
+                print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Extracted #{Utils::RESET_COLOR}#{file_path}\n" if $global_options[:verbose]
+            end
+
             if with_scripts_separate
                 # 去除 nil 元素
-                scripts_strings.compact!
-                Utils.object_json(scripts_strings.each_with_object({}) { |item, h| h[item] = item }, File.join(output_dir, 'ManualTransFile_scripts.json'))
+                all_rb_strings.compact!
+                Utils.object_json(all_rb_strings.each_with_object({}) { |item, h| h[item] = item }, output_dir.join('ManualTransFile_scripts.json'))
             else
-                all_ex_strings.concat(scripts_strings)
+                all_ex_strings.concat(all_rb_strings)
             end
         end
 
         # 去除 nil 元素
         all_ex_strings.compact!
-        Utils.object_json(all_ex_strings.each_with_object({}) { |item, h| h[item] = item }, File.join(output_dir, "ManualTransFile.json"))
+        Utils.object_json(all_ex_strings.each_with_object({}) { |item, h| h[item] = item }, output_dir.join('ManualTransFile.json'))
     end
 
 end
