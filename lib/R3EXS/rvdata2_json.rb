68ed358d8923a71f2f80829c94ac616d35e81401
LuoTat <LuoTat.s@gmail.com>
2025-04-09 00:09:38 +0800

    为了支持递归遍历所有的*.rvdata2文件，修改了函数rvdata2_json的逻辑，同时添加了commonevents_json函数来将CommonEvents对象数组序列化为分开的JSON文件

--- lib/R3EXS/rvdata2_json.rb
+++ lib/R3EXS/rvdata2_json.rb
@@ -8,32 +8,65 @@ module R3EXS
 
     # 将 Script 对象数组序列化为 Ruby 源码
     #
-    # @param scripts [Array<Array>] 待转换的 Script 对象数组
-    # @param output_dir [String] 输出目录
+    # @param scripts [Object] 待转换的 Script 对象数组
+    # @param output_dir [Pathname] 输出目录
     # @return [void]
     def R3EXS.scripts_rb(scripts, output_dir)
-        FileUtils.mkdir(output_dir) unless Dir.exist?(output_dir)
-        full_dir = File.join(output_dir, 'Scripts')
-        FileUtils.mkdir(full_dir) unless Dir.exist?(full_dir)
+        output_dir.mkpath unless output_dir.exist?
+        full_dir = output_dir.join('Scripts')
+        full_dir.mkdir unless full_dir.exist?
 
         scripts_info_array = []
         scripts.each_with_index do |script, index|
             next if script.nil?
             scripts_info_array << { index: index, name: script[1] }
-            script_file_path = File.join(full_dir, "#{format('%03d', index)}.rb")
+            script_file_path = full_dir.join("#{format('%03d', index)}.rb")
             print "#{Utils::ESCAPE}#{Utils::MAGENTA_COLOR}Serializing to #{Utils::RESET_COLOR}#{script_file_path}...\r" if $global_options[:verbose]
-            File.write(script_file_path, Zlib::Inflate.inflate(script[2]).encode(universal_newline: true), mode: 'w')
+            script_file_path.write(Zlib::Inflate.inflate(script[2]).encode(universal_newline: true))
+            print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Serialized #{Utils::RESET_COLOR}#{script_file_path}\n" if $global_options[:verbose]
         end
 
-        script_info_file_path = File.join(full_dir, 'Scripts_info.json')
+        script_info_file_path = full_dir.join('Scripts_info.json')
         print "#{Utils::ESCAPE}#{Utils::MAGENTA_COLOR}Serializing to #{Utils::RESET_COLOR}#{script_info_file_path}\r" if $global_options[:verbose]
         Utils.object_json(scripts_info_array, script_info_file_path)
+        print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Serialized #{Utils::RESET_COLOR}#{script_info_file_path}\n" if $global_options[:verbose]
+    end
+
+    # 将 CommonEvents 对象数组序列化为分开的 JSON 文件
+    #
+    # @param commonevents [Object] 待转换的 CommonEvents 对象数组
+    # @param output_dir [Pathname] 输出目录
+    # @param complete [Boolean] 是否序列化所有内容
+    # @param with_notes [Boolean] 是否包含备注
+    # @return [void]
+    def R3EXS.commonevents_json(commonevents, output_dir, complete, with_notes)
+        output_dir.mkpath unless output_dir.exist?
+        full_dir = output_dir.join('CommonEvents')
+        full_dir.mkdir unless full_dir.exist?
+
+        if complete
+            commonevents.each_with_index do |commonevent, index|
+                commonevent_file_path = full_dir.join("#{format('CommonEvent_%05d', index)}.json")
+                print "#{Utils::ESCAPE}#{Utils::MAGENTA_COLOR}Serializing to #{Utils::RESET_COLOR}#{commonevent_file_path}...\r" if $global_options[:verbose]
+                commonevent_file_path.write(Oj.dump(commonevent, indent: 2))
+                print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Serialized #{Utils::RESET_COLOR}#{commonevent_file_path}\n" if $global_options[:verbose]
+            end
+        else
+            commonevents = Utils.rpg_r3exs(commonevents, 'CommonEvents', with_notes)
+            commonevents.each do |commonevent|
+                index                 = commonevent.index
+                commonevent_file_path = full_dir.join("#{format('CommonEvent_%05d', index)}.json")
+                print "#{Utils::ESCAPE}#{Utils::MAGENTA_COLOR}Serializing to #{Utils::RESET_COLOR}#{commonevent_file_path}...\r" if $global_options[:verbose]
+                commonevent_file_path.write(Oj.dump(commonevent, indent: 2))
+                print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Serialized #{Utils::RESET_COLOR}#{commonevent_file_path}\n" if $global_options[:verbose]
+            end
+        end
     end
 
     # 将指定目录下的所有 rvdata2 文件序列化为 JSON 格式
     #
-    # @param target_dir [String] 目标目录
-    # @param output_dir [String] 输出目录
+    # @param target_dir [Pathname] 目标目录
+    # @param output_dir [Pathname] 输出目录
     # @param complete [Boolean] 是否序列化所有内容
     # @param with_scripts [Boolean] 是否包含脚本
     # @param with_notes [Boolean] 是否包含备注
@@ -43,21 +76,20 @@ module R3EXS
     #
     # @return [void]
     def R3EXS.rvdata2_json(target_dir, output_dir, complete, with_scripts, with_notes)
-        FileUtils.mkdir(output_dir) unless Dir.exist?(output_dir)
-        Utils.all_rvdata2_files(target_dir) do |object, file_basename|
-            if file_basename == 'Scripts'
-                next unless with_scripts
-                scripts_rb(object, output_dir)
-                print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Serialized #{Utils::RESET_COLOR}#{file_basename}\n" if $global_options[:verbose]
+        Utils.all_rvdata2_files(target_dir) do |object, file_basename, parent_relative_dir|
+            if file_basename == 'Scripts' && with_scripts
+                scripts_rb(object, output_dir.join(parent_relative_dir))
+            elsif file_basename == 'CommonEvents'
+                commonevents_json(object, output_dir.join(parent_relative_dir), complete, with_notes)
             else
-                file_path = File.join(output_dir, "#{file_basename}.json")
+                file_path = output_dir.join(parent_relative_dir, "#{file_basename}.json")
                 print "#{Utils::ESCAPE}#{Utils::MAGENTA_COLOR}Serializing to #{Utils::RESET_COLOR}#{file_path}...\r" if $global_options[:verbose]
                 if complete
                     Utils.object_json(object, file_path)
                 else
                     Utils.object_json(Utils.rpg_r3exs(object, file_basename, with_notes), file_path)
                 end
-                print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Serialized #{Utils::RESET_COLOR}#{file_basename}\n" if $global_options[:verbose]
+                print "#{Utils::ESCAPE}#{Utils::GREEN_COLOR}Serialized #{Utils::RESET_COLOR}#{file_path}\n" if $global_options[:verbose]
             end
         end
     end
