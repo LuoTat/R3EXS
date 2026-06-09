cb974b8c496cc6095e51935266b6e273ac3de937
LuoTat <LuoTat.s@gmail.com>
2026-06-09 15:33:13 +0800

    处理Windows平台上的编译

--- ext/R3EXS/extconf.rb
+++ ext/R3EXS/extconf.rb
@@ -1,5 +1,10 @@
 require 'mkmf'
 
-$ARCH_FLAG << '-march=native -std=c++23'
+$CXXFLAGS << ' -std=c++23' # 添加对C++23的支持
+
+if CONFIG['host_os'] =~ /mswin|mingw|cygwin/
+  $CXXFLAGS << ' -march=native' # 启用针对本地CPU的优化
+  have_library('stdc++exp') # 在Windows上链接stdc++exp库以支持C++23特性
+end
 
 create_makefile('R3EXS/R3EXS')
