30d6f79d0028a3d47c95239781536f6a9d9c28a5
LuoTat <LuoTat.s@gmail.com>
2026-06-13 16:30:13 +0800

    移动了几个函数的位置

--- lib/R3EXS/rvdata2_json.rb
+++ lib/R3EXS/rvdata2_json.rb
@@ -5,6 +5,36 @@ require_relative 'utils'
 require_relative 'logger'
 
 module R3EXS
+  # 将 CommonEvents 对象数组序列化为分开的 JSON 文件
+  #
+  # @param commonevents [Array<RPG::CommonEvent>] 待转换的 CommonEvents 对象数组
+  # @param output_dir [Pathname] 输出目录
+  # @param complete [Boolean] 是否序列化所有内容
+  # @param with_notes [Boolean] 是否包含备注
+  #
+  # @return [void]
+  def self.commonevents_json(commonevents, output_dir, complete, with_notes)
+    full_dir = output_dir.join('CommonEvents')
+
+    if complete
+      commonevents.each_with_index do |commonevent, index|
+        commonevent_file_path = full_dir.join("#{format('CommonEvent_%05d', index)}.json")
+        Logger.debug("Serializing to #{commonevent_file_path}...")
+        Utils.object_json(commonevent, commonevent_file_path)
+        Logger.debug("Serialized #{commonevent_file_path}")
+      end
+    else
+      commonevents = Utils.rpg_r3exs(commonevents, 'CommonEvents', with_notes)
+      commonevents.each do |commonevent|
+        index = commonevent.index
+        commonevent_file_path = full_dir.join("#{format('CommonEvent_%05d', index)}.json")
+        Logger.debug("Serializing to #{commonevent_file_path}...")
+        Utils.object_json(commonevent, commonevent_file_path)
+        Logger.debug("Serialized #{commonevent_file_path}")
+      end
+    end
+  end
+
   # 将 Script 对象数组序列化为 Ruby 源码
   #
   # @param scripts [Array<Object>] 待转换的 Script 对象数组
@@ -33,36 +63,6 @@ module R3EXS
     Logger.debug("Serialized #{script_info_file_path}")
   end
 
-  # 将 CommonEvents 对象数组序列化为分开的 JSON 文件
-  #
-  # @param commonevents [Array<Object>] 待转换的 CommonEvents 对象数组
-  # @param output_dir [Pathname] 输出目录
-  # @param complete [Boolean] 是否序列化所有内容
-  # @param with_notes [Boolean] 是否包含备注
-  #
-  # @return [void]
-  def self.commonevents_json(commonevents, output_dir, complete, with_notes)
-    full_dir = output_dir.join('CommonEvents')
-
-    if complete
-      commonevents.each_with_index do |commonevent, index|
-        commonevent_file_path = full_dir.join("#{format('CommonEvent_%05d', index)}.json")
-        Logger.debug("Serializing to #{commonevent_file_path}...")
-        Utils.object_json(commonevent, commonevent_file_path)
-        Logger.debug("Serialized #{commonevent_file_path}")
-      end
-    else
-      commonevents = Utils.rpg_r3exs(commonevents, 'CommonEvents', with_notes)
-      commonevents.each do |commonevent|
-        index = commonevent.index
-        commonevent_file_path = full_dir.join("#{format('CommonEvent_%05d', index)}.json")
-        Logger.debug("Serializing to #{commonevent_file_path}...")
-        Utils.object_json(commonevent, commonevent_file_path)
-        Logger.debug("Serialized #{commonevent_file_path}")
-      end
-    end
-  end
-
   # 将指定目录下的所有 rvdata2 文件序列化为 JSON 格式
   #
   # @param target_dir [Pathname] 目标目录
@@ -77,12 +77,13 @@ module R3EXS
   # @return [void]
   def self.rvdata2_json(target_dir, output_dir, complete, with_scripts, with_notes)
     Utils.all_rvdata2_files(target_dir) do |object, file_basename, parent_relative_dir|
-      if file_basename == 'Scripts'
-        scripts_rb(object, output_dir.join(parent_relative_dir)) if with_scripts
-      elsif file_basename == 'CommonEvents'
-        commonevents_json(object, output_dir.join(parent_relative_dir), complete, with_notes)
+      full_output_dir = output_dir.join(parent_relative_dir)
+      if file_basename == 'CommonEvents'
+        commonevents_json(object, full_output_dir, complete, with_notes)
+      elsif with_scripts && (file_basename == 'Scripts')
+        scripts_rb(object, full_output_dir)
       else
-        file_path = output_dir.join(parent_relative_dir, "#{file_basename}.json")
+        file_path = full_output_dir.join("#{file_basename}.json")
         Logger.debug("Serializing to #{file_path}...")
         if complete
           Utils.object_json(object, file_path)
