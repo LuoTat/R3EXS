1923dfeabb1e52610bb6536143c0af2e7d654d2d
LuoTat <LuoTat.s@gmail.com>
2026-06-13 19:55:59 +0800

    将原本的一个函数拆分为3个子函数

--- lib/R3EXS/extract_strings.rb
+++ lib/R3EXS/extract_strings.rb
@@ -6,7 +6,46 @@ require_relative 'utils'
 require_relative 'logger'
 
 module R3EXS
-  # 将指定目录下的所有已经序列化为 R3EXS 后的 JOSN 文件中的字符串提取出来
+  # 提取已序列化为 R3EXS 格式的 CommonEvent 文件中的字符串
+  #
+  # @param target_dir [Pathname] 目标目录
+  #
+  # @return [Array<String>]
+  def self.ex_commonevents(target_dir)
+    ex_strings = []
+    Utils.all_commonevent_json_files(target_dir, :R3EXS) do |commonevents, commonevents_basenames, parent_relative_dir|
+      commonevents.zip(commonevents_basenames).each do |commonevent, commonevent_basename|
+        file_path = target_dir.join(parent_relative_dir, "#{commonevent_basename}.json")
+        Logger.debug("Extracting from #{file_path}...")
+        ex_strings.concat(commonevent.ex_strings)
+        Logger.debug("Extracted #{file_path}")
+      end
+    end
+    ex_strings
+  end
+
+  # 提取 Ruby 源码中的字符串
+  #
+  # @param target_dir [Pathname] 目标目录
+  # @param with_symbol [Boolean] 是否包含脚本中的符号
+  #
+  # @return [Array<String>]
+  def self.ex_scripts(target_dir, with_symbol)
+    ex_strings = []
+    strings_extractor = StringsExtractor.new(ex_strings, with_symbol)
+
+    Utils.all_rb_files(target_dir) do |scripts, scripts_basenames, parent_relative_dir|
+      scripts.zip(scripts_basenames).each do |script, script_basename|
+        file_path = target_dir.join(parent_relative_dir, "#{script_basename}.rb")
+        Logger.debug("Extracting from #{file_path}...")
+        strings_extractor.visit(Prism.parse(script).value)
+        Logger.debug("Extracted #{file_path}")
+      end
+    end
+    ex_strings
+  end
+
+  # 提取指定目录下所有已序列化为 R3EXS 格式的 JOSN 文件中的字符串
   #
   # @param target_dir [Pathname] 目标目录
   # @param output_dir [Pathname] 输出目录
@@ -21,55 +60,34 @@ module R3EXS
   def self.ex_strings(target_dir, output_dir, with_scripts, with_symbol, with_scripts_separate)
     all_ex_strings = []
 
-    # 处理常规的 JSON 文件
-    Utils.all_json_files(target_dir, :R3EXS) do |object, file_basename, parent_relative_dir|
-      file_path = target_dir.join(parent_relative_dir, "#{file_basename}.json")
-        Logger.debug("Extracting from #{file_path}...")
-      if object.is_a?(Array)
-        object.each do |obj|
-          all_ex_strings.concat(obj.ex_strings)
-        end
-      else
-        all_ex_strings.concat(object.ex_strings)
-      end
-        Logger.debug("Extracted #{file_path}")
-    end
-
     # 处理 CommonEvent_\d{5}.json 文件
-    Utils.all_commonevent_json_files(target_dir, :R3EXS) do |commonevents, commonevents_basenames, parent_relative_dir|
-      commonevents.zip(commonevents_basenames).each do |commonevent, commonevent_basename|
-        file_path = target_dir.join(parent_relative_dir, "#{commonevent_basename}.json")
-          Logger.debug("Extracting from #{file_path}...")
-        all_ex_strings.concat(commonevent.ex_strings)
-          Logger.debug("Extracted #{file_path}")
-      end
-    end
+    all_ex_strings.concat(ex_commonevents(target_dir))
 
     # 处理 \d{5}.rb 文件
     if with_scripts
-      all_rb_strings = []
-      strings_extractor = StringsExtractor.new(all_rb_strings, with_symbol)
-
-      Utils.all_rb_files(target_dir) do |scripts, scripts_basenames, parent_relative_dir|
-        scripts.zip(scripts_basenames).each do |script, script_basename|
-          file_path = target_dir.join(parent_relative_dir, "#{script_basename}.rb")
-            Logger.debug("Extracting from #{file_path}...")
-          strings_extractor.visit(Prism.parse(script).value)
-            Logger.debug("Extracted #{file_path}")
-        end
-      end
-
+      rb_strings = ex_scripts(target_dir, with_symbol)
+      # 单独输出脚本提取的字符串
       if with_scripts_separate
-        # 去除 nil 元素
-        all_rb_strings.compact!
-        Utils.object_json(all_rb_strings.each_with_object({}) { |item, h| h[item] = item }, output_dir.join('ManualTransFile_scripts.json'))
+        Utils.object_json(rb_strings.to_h { |str| [str, str] }, output_dir.join('ManualTransFile_scripts.json'))
       else
-        all_ex_strings.concat(all_rb_strings)
+        all_ex_strings.concat(rb_strings)
       end
+    end
+
+    # 处理常规的 JSON 文件
+    Utils.all_json_files(target_dir, :R3EXS) do |object, file_basename, parent_relative_dir|
+      file_path = target_dir.join(parent_relative_dir, "#{file_basename}.json")
+      Logger.debug("Extracting from #{file_path}...")
+      if object.is_a?(Array)
+        object.each { |obj| all_ex_strings.concat(obj.ex_strings) }
+      else
+        all_ex_strings.concat(object.ex_strings)
+      end
+      Logger.debug("Extracted #{file_path}")
     end
 
     # 去除 nil 元素
     all_ex_strings.compact!
-    Utils.object_json(all_ex_strings.each_with_object({}) { |item, h| h[item] = item }, output_dir.join('ManualTransFile.json'))
+    Utils.object_json(all_ex_strings.to_h { |str| [str, str] }, output_dir.join('ManualTransFile.json'))
   end
 end
