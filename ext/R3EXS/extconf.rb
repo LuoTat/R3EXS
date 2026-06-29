7d4acff332da3260f0409d335a934810bf168fbe
LuoTat <LuoTat.s@gmail.com>
2026-06-30 01:18:20 +0800

    添加-O3优化，因为CXXFLAGS可能没有优化选项

--- ext/R3EXS/extconf.rb
+++ ext/R3EXS/extconf.rb
@@ -3,7 +3,7 @@
 require 'mkmf'
 
 $ARCH_FLAG = '-march=native'
-$CXXFLAGS << ' -std=c++23' # 添加对C++23的支持
+$CXXFLAGS << ' -O3 -std=c++23' # 添加对C++23的支持
 
 if RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
   have_library('stdc++exp') # 在Windows上链接stdc++exp库以支持C++23特性
