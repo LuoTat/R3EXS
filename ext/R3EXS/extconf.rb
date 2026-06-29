019444570bea33b6fd0cc5aa65b969e39e72d209
LuoTat <LuoTat.s@gmail.com>
2026-06-29 23:53:20 +0800

    修复了平台判定代码

--- ext/R3EXS/extconf.rb
+++ ext/R3EXS/extconf.rb
@@ -4,7 +4,7 @@ require 'mkmf'
 
 $CXXFLAGS << ' -std=c++23' # 添加对C++23的支持
 
-if CONFIG['host_os'] =~ /mswin|mingw|cygwin/
+if RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
   $CXXFLAGS << ' -march=native' # 启用针对本地CPU的优化
   have_library('stdc++exp') # 在Windows上链接stdc++exp库以支持C++23特性
 end
