2e6764b41e6f7a74264315484c751c5c7d4e5d82
LuoTat <LuoTat.s@gmail.com>
2026-06-30 00:24:37 +0800

    全平台启用针对本地CPU的优化

--- ext/R3EXS/extconf.rb
+++ ext/R3EXS/extconf.rb
@@ -2,10 +2,10 @@
 
 require 'mkmf'
 
+$ARCH_FLAG = '-march=native'
 $CXXFLAGS << ' -std=c++23' # 添加对C++23的支持
 
 if RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
-  $CXXFLAGS << ' -march=native' # 启用针对本地CPU的优化
   have_library('stdc++exp') # 在Windows上链接stdc++exp库以支持C++23特性
 end
 
